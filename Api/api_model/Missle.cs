998f77745ccc4f8d791a368a005feb26e00dc6e1
m <m0548572355@gmail.com>
2023-11-07 00:47:39 +0200

    m

---
+++ Api/api_model/Missle.cs
@@ -0,0 +1,8 @@
+﻿namespace api_model
+{
+    public class Missle
+    {
+        public string Patriotmissile { get; set; }
+        public Location Location { get; set; } 
+    }
+}
---
+++ Api/api_service/MissleService.cs
@@ -0,0 +1,42 @@
+﻿using api_model;
+
+namespace api_service
+{
+    public class MissleService
+    {
+        readonly Data _db;
+        public MissleService(Data data)
+        {
+            this._db = data;
+        }
+        //מחזיר את מערך הטילים
+        public IEnumerable<Missle> GetData()
+        {
+            return _db.data;
+        }
+        public bool add(Missle m)
+        {
+            if(_db.data.Contains(m))
+            {
+                return false;
+            }
+            else
+            {
+                _db.data.Add(m);
+                return true;
+            }
+        }
+        //החזרת טילים לפי מיקום
+        public IEnumerable<Missle> GetDataByCity(string city)
+        {
+            return _db.data.Where(t => t.Location.City == city).ToList();
+        }
+
+        public IEnumerable<string> GetCities()
+        {
+            return _db.data.Select(x => x.Location.City).ToList().Distinct();
+
+        }
+
+    }
+}
